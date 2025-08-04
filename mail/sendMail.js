278a59ae350efdf04212ae3fc3b74ce5f3bf1132
Yassine Echadani <yassinechadani113@gmail.com>
2025-08-04 15:33:49 +0100

    refactoer: Improve mail sending functionality

--- mail/sendMail.js
+++ mail/sendMail.js
@@ -1,30 +1,30 @@
-const nodemailer = require('nodemailer')
-require('dotenv').config()
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for port 465, false for 587
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-})
+});
 
 async function sendMail({ name, email, subject, message }) {
   const mailOptions = {
     from: `"${name}" <${email}>`,
     to: process.env.RECEIVER_EMAIL,
-    subject: subject,
+    subject,
     text: `
-      Name: ${name}
-      Email: ${email}
-      Subject: ${subject}
-      Message: ${message}
+Name: ${name}
+Email: ${email}
+Subject: ${subject}
+Message: ${message}
     `,
-  }
+  };
 
-  return transporter.sendMail(mailOptions)
+  return transporter.sendMail(mailOptions);
 }
 
-module.exports = sendMail
+module.exports = sendMail;
